687005500dd1d4a2b195a8877e8fb9a89fcbbb93
Stefano Picozzi <stefanopicozzi@dhcp-128-175.syd.redhat.com>
2015-12-07 15:14:15 +1100

    x

--- tools/cURL/post-facts.sh
+++ tools/cURL/post-facts.sh
@@ -1,2 +1,2 @@
 
-curl -i -H "Content-Type: application/xml" -H "X-KIE-ContentType: XSTREAM" -d @"post-facts.xml" -u "erics:jbossbrms1!" -X POST "http://weightwatcher2.cloudapps.example.com/kie-server/services/rest/server/containers/instances/watch"
+curl -i -H "Content-Type: application/xml" -H "X-KIE-ContentType: XSTREAM" -d @"post-facts.xml" -u "erics:jbossbrms1!" -X POST "http://weightwatcher.cloudapps.example.com/kie-server/services/rest/server/containers/instances/watch"
